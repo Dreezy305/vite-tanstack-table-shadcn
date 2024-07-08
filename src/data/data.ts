bf484ac76e65b99b0803bc527f951c5afb88abc2
dreezy305 <bankoleidris@gmail.com>
2024-07-09 00:32:38 +0100

    data sets of 200 people generated

---
+++ src/data/data.ts
@@ -0,0 +1,32 @@
+import { faker } from "@faker-js/faker";
+
+export type Person = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  visits: number;
+  progress: number;
+  status: "relationship" | "complicated" | "single";
+  subRows?: Person[];
+};
+
+const statuses = ["relationship", "complicated", "single"];
+
+const generatePerson = (): Person => {
+  return {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    age: faker.datatype.number({ min: 18, max: 80 }),
+    visits: faker.datatype.number({ min: 0, max: 1000 }),
+    progress: faker.datatype.number({ min: 0, max: 100 }),
+    status: statuses[faker.datatype.number({ min: 0, max: 2 })] as
+      | "relationship"
+      | "complicated"
+      | "single",
+    subRows: [],
+  };
+};
+
+export const generatePeople = (num: number): Person[] => {
+  return Array.from({ length: num }, generatePerson);
+};
